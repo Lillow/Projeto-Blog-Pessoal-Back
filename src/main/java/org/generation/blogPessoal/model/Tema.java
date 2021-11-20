e0366509a19b8b10aad2c780dc7ed50002c4b69a
Danillo Silva <danillordm@gmail.com>
2021-11-20 11:57:03 -0300

    @Lillow [task: corrections in the models]

--- src/main/java/org/generation/blogPessoal/model/Tema.java
+++ src/main/java/org/generation/blogPessoal/model/Tema.java
@@ -1,5 +1,6 @@
 package org.generation.blogPessoal.model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.CascadeType;
@@ -24,9 +25,10 @@ public class Tema {
 	@NotNull
 	private String descricao;
 
-	@OneToMany(mappedBy = "tema", cascade = CascadeType.ALL)
+	@OneToMany(mappedBy = "tema", cascade = CascadeType.REMOVE)
 	@JsonIgnoreProperties("tema")
-	private List<Postagem> postagem;
+	private List<Postagem> postagem = new ArrayList<>();
+
 
 	public long getId() {
 		return id;
--- src/main/java/org/generation/blogPessoal/model/Usuario.java
+++ src/main/java/org/generation/blogPessoal/model/Usuario.java
@@ -1,13 +1,20 @@
 package org.generation.blogPessoal.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import io.swagger.annotations.ApiModelProperty;
 
@@ -23,6 +30,10 @@ public class Usuario {
 	@Size(min = 5, max = 100)
 	private String nome;
 
+	@NotNull
+	@Size(min = 5, max = 100)
+	private String foto;
+	
 	@ApiModelProperty(example = "email@email.com.br")
 	@NotNull(message = "O atributo Usuário é Obrigatório!")
 	@Email(message = "O atributo Usuário deve ser um email válido!")
@@ -32,6 +43,10 @@ public class Usuario {
 	@NotNull
 	@Size(min = 5)
 	private String senha;
+
+	@OneToMany(mappedBy = "criador", cascade = CascadeType.REMOVE)
+	@JsonIgnoreProperties({ "criador" })
+	private List<Postagem> minhasPostagens = new ArrayList<>();
 
 	public long getId() {
 		return id;
@@ -49,6 +64,14 @@ public class Usuario {
 		this.nome = nome;
 	}
 
+	public String getFoto() {
+		return foto;
+	}
+
+	public void setFoto(String foto) {
+		this.foto = foto;
+	}
+
 	public String getUsuario() {
 		return usuario;
 	}
@@ -63,6 +86,14 @@ public class Usuario {
 
 	public void setSenha(String senha) {
 		this.senha = senha;
+	}
+
+	public List<Postagem> getMinhasPostagens() {
+		return minhasPostagens;
+	}
+
+	public void setMinhasPostagens(List<Postagem> minhasPostagens) {
+		this.minhasPostagens = minhasPostagens;
 	}
 
 }
