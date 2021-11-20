144cae6bda91a098c2926a0fc5720e765d7e8a19
Danillo Silva <danillordm@gmail.com>
2021-11-20 12:05:25 -0300

    @Lillow [task: fix in userLogin]

--- src/main/java/org/generation/blogPessoal/model/UserLogin.java
+++ src/main/java/org/generation/blogPessoal/model/UserLogin.java
@@ -2,13 +2,20 @@ package org.generation.blogPessoal.model;
 
 public class UserLogin {
 
+	private long id;
 	private String nome;
-
 	private String usuario;
-
+	private String foto;
 	private String senha;
-
 	private String token;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
 
 	public String getNome() {
 		return nome;
@@ -22,16 +29,16 @@ public class UserLogin {
 		return usuario;
 	}
 
-	public String getToken() {
-		return token;
-	}
-
-	public void setToken(String token) {
-		this.token = token;
-	}
-
 	public void setUsuario(String usuario) {
 		this.usuario = usuario;
+	}
+
+	public String getFoto() {
+		return foto;
+	}
+
+	public void setFoto(String foto) {
+		this.foto = foto;
 	}
 
 	public String getSenha() {
@@ -41,4 +48,13 @@ public class UserLogin {
 	public void setSenha(String senha) {
 		this.senha = senha;
 	}
+
+	public String getToken() {
+		return token;
+	}
+
+	public void setToken(String token) {
+		this.token = token;
+	}
+
 }
