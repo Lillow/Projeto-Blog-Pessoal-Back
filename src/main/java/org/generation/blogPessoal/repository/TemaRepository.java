ed56e10defe70e23fb4a93adad7164fe94908460
Danillo Silva <danillordm@gmail.com>
2021-11-22 12:01:39 -0300

    @Lillow [task: repositories fixes]

--- src/main/java/org/generation/blogPessoal/repository/TemaRepository.java
+++ src/main/java/org/generation/blogPessoal/repository/TemaRepository.java
@@ -5,7 +5,9 @@ import java.util.List;
 import org.generation.blogPessoal.model.Tema;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TemaRepository extends JpaRepository<Tema, Long> {
 	public List<Tema> findAllByDescricaoContainingIgnoreCase(String descricao);
 }
--- src/main/java/org/generation/blogPessoal/service/UsuarioService.java
+++ src/main/java/org/generation/blogPessoal/service/UsuarioService.java
@@ -31,46 +31,46 @@ public class UsuarioService {
 		if (usuarioRepository.findById(usuario.getId()).isPresent()) {
 			Optional<Usuario> buscaUsuario = usuarioRepository.findByUsuario(usuario.getUsuario());
 
-			if (buscaUsuario.isPresent()) {				
+			if (buscaUsuario.isPresent()) {
 				if (buscaUsuario.get().getId() != usuario.getId())
 					throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Usuário já existe!", null);
 			}
-			
+
 			usuario.setSenha(criptografarSenha(usuario.getSenha()));
 			return Optional.of(usuarioRepository.save(usuario));
-		} 
-		throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Usuário não encontrado!", null);		
-	}	
-	
+		}
+		throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Usuário não encontrado!", null);
+	}
+
 	public Optional<UserLogin> logarUsuario(Optional<UserLogin> usuarioLogin) {
 		Optional<Usuario> usuario = usuarioRepository.findByUsuario(usuarioLogin.get().getUsuario());
 
 		if (usuario.isPresent()) {
 			if (compararSenhas(usuarioLogin.get().getSenha(), usuario.get().getSenha())) {
-				usuarioLogin.get().setId(usuario.get().getId());				
+				usuarioLogin.get().setId(usuario.get().getId());
 				usuarioLogin.get().setNome(usuario.get().getNome());
 				usuarioLogin.get().setFoto(usuario.get().getFoto());
 				usuarioLogin.get().setSenha(usuario.get().getSenha());
-				usuarioLogin.get().setToken(generatorBasicToken(usuarioLogin.get().getUsuario(), usuarioLogin.get().getSenha()));
+				usuarioLogin.get()
+						.setToken(generatorBasicToken(usuarioLogin.get().getUsuario(), usuarioLogin.get().getSenha()));
 
 				return usuarioLogin;
 			}
-		}		
-		throw new ResponseStatusException(
-				HttpStatus.UNAUTHORIZED, "Usuário ou senha inválidos!", null);
+		}
+		throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Usuário ou senha inválidos!", null);
 	}
-	
+
 	private String criptografarSenha(String senha) {
 		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 		String senhaEncoder = encoder.encode(senha);
 		return senhaEncoder;
 	}
-	
+
 	private boolean compararSenhas(String senhaDigitada, String senhaBanco) {
 		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-		return encoder.matches(senhaDigitada, senhaBanco);		
+		return encoder.matches(senhaDigitada, senhaBanco);
 	}
-	
+
 	private String generatorBasicToken(String email, String password) {
 		String structure = email + ":" + password;
 		byte[] structureBase64 = Base64.encodeBase64(structure.getBytes(Charset.forName("US-ASCII")));
